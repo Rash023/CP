123d8e2588d51e81b179415b9d56328b273b5e23
Rash023 <rashidmazhar0507@gmail.com>
2024-08-25 22:03:06 +0530

    Added Codeforces Round 968

---
+++ Codeforces/Codeforces Round 968 (Div 2)/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fl(i, n) for (int i = 0; i < n; i++)
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{
+    return (istream >> p.first >> p.second);
+}
+template <typename T> // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T1, typename T2> // cout >> pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T> // cout >> vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void play(stack<pair<int, int>> &st, vector<int> &a, bool flag, int prev)
+{
+}
+
+void solve()
+{
+    // Write your code here
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(begin(a) + 1, end(a));
+    cout << a[n / 2 + 1] << endl;
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t; // Uncomment if there are multiple test cases
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
